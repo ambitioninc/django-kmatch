036ed427b621ed07bc1251fb3427c7b40bc4d9cd
Jeffrey Cross <jeffrey.cross@ambition.com>
2023-05-30 21:15:34 -0400

    Commented field print statements

--- django_kmatch/fields.py
+++ django_kmatch/fields.py
@@ -37,17 +37,17 @@ class Creator(object):
     A placeholder class that provides a way to set the attribute on the model.
     """
     def __init__(self, field):
-        print('hello from Creator.__init__!')
+        # print('hello from Creator.__init__!')
         self.field = field
 
     def __get__(self, obj, type=None):
-        print('hello from Creator.__get__!')
+        # print('hello from Creator.__get__!')
         if obj is None:
             raise AttributeError('Can only be accessed via an instance.')
         return obj.__dict__[self.field.name]
 
     def __set__(self, obj, value):
-        print('hello from Creator.__set__!')
+        # print('hello from Creator.__set__!')
         # Usually this would call to_python, but we've changed it to pre_init
         # so that we can tell which state we're in. By passing an obj,
         # we can definitively tell if a value has already been deserialized
@@ -64,7 +64,7 @@ class KField2(DjangoJSONField):
     description = 'A kmatch pattern'
 
     def __init__(self, *args, **kwargs):
-        print('hello from KField2.__init__!')
+        # print('hello from KField2.__init__!')
         self.dump_kwargs = kwargs.pop('dump_kwargs', {
             'cls': DjangoJSONEncoder,
             'separators': (',', ':')
@@ -77,7 +77,7 @@ class KField2(DjangoJSONField):
         """
         Converts a K object to a pattern.
         """
-        print('hello from KField2.get_db_prep_value!')
+        # print('hello from KField2.get_db_prep_value!')
         if isinstance(value, K):
             value = value.pattern
         return super(KField2, self).get_db_prep_value(value, connection, prepared=False)
@@ -88,10 +88,10 @@ class KField2(DjangoJSONField):
         of a Django custom field. However, this field inherits JSONField, and JSONField had to do
         conversions in the pre_init method.
         """
-        print('hello from KField2.pre_init!')
+        # print('hello from KField2.pre_init!')
         return K(value) if not isinstance(value, K) and value is not None else value
 
     def contribute_to_class(self, cls, name, private_only=False):
-        print('hello from KField2.contribute_to_class!')
+        # print('hello from KField2.contribute_to_class!')
         super().contribute_to_class(cls, name)
         setattr(cls, name, Creator(self))
